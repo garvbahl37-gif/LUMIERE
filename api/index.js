92ffb9542a94cfb930bcbf403166a128a501f08d
garvbahl37-gif <garvbahl37@gmail.com>
2025-12-28 14:13:07 +0530

    chore(debug): wrap api entry point in handler with request logging

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
 import app from '../server/server.js';
 
-export default app;
+export default async function handler(req, res) {
+    console.log('Vercel API Handler:', req.method, req.url);
+
+    // Serve as proxy to Express app
+    return app(req, res);
+}
